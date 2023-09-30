f1d723864b87ee2fd01ab710f75b954d458a9162
rasha-massoud <rashamassoud1998@gmail.com>
2023-09-30 21:11:35 +0300

    Added the registeration function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,37 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Auth User"})
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Register User"})
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists){
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create ({
+        name,
+        email,
+        password
+    });
+
+    if (user) {        
+        res.status(201).json({ 
+            _id: uswer._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ connectDB();
 const port = process.env.PORT;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}));
+
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send("Server is ready"));
