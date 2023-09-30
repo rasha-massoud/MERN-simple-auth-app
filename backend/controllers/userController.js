e9e3d79dd76998d78418661eb655eb381e98e1a0
rasha-massoud <rashamassoud1998@gmail.com>
2023-09-30 15:49:48 +0300

    Installed the express-async-handler dependency for try..catcg purposes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
-const authUser = (req, res) => {
+import asyncHandler from "express-async-handler";
+
+const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Auth User"})
-}
+});
 
 export {
     authUser
