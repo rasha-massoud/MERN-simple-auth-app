15dd7651dc200a76147e449b8ca8c87ef091a022
rasha-massoud <rashamassoud1998@gmail.com>
2023-09-30 15:47:02 +0300

    Created the authUser function for testing

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT;
-
 const app = express();
+
+import userRoutes from "./routes/userRoutes.js";
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send("Server is ready"));
 
