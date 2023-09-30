be6d136f02eeae60f3217e805e643e850ae779ad
rasha-massoud <rashamassoud1998@gmail.com>
2023-09-30 16:11:25 +0300

    Added the routes for the users' functions in the userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile } from "../controllers/userController.js";
 
-router.post('/auth', authUser);
+router.post('/', authUser);
+router.post('/auth', registerUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
