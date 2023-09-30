8adf5796a79b0d964b08cdf638f00807041879e0
rasha-massoud <rashamassoud1998@gmail.com>
2023-09-30 16:10:37 +0300

    Created draft functions for users in the userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,26 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Auth User"})
 });
 
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Register User"})
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Logout User"})
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "User Profile"})
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Update User Profile"})
+});
+
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 }
