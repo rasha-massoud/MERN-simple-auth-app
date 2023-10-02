ee4fae9f2e1bd517449e9f4e9d2473ddd3d2fbc1
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-02 12:00:03 +0300

    Updated the login function to include the match password condition

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,11 +3,11 @@ import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 const authUser = asyncHandler(async (req, res) => {
-    const { name, email } = req.body;
+    const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    
-    if (user) {     
+
+    if (user && (await user.matchPasswords(password))) {     
         generateToken(res, user._id );  
         res.status(201).json({ 
             _id: user._id,
@@ -47,7 +47,6 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(401);
         throw new Error("Invalid user data");
     }
-
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
