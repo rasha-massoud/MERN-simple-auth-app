031d575431a66d3ea3c0d20ad45fbf5c9c7a2e03
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-06 19:27:42 +0300

    Imported the Header to the App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header"
+
 const App = () => {
   return (
     <>
+      <Header/>
       <h1>MERN Simple Auth App</h1>
     </>
   )
 }
 
-export default App
+export default App;
