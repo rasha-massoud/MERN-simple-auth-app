09d866bfabfd1309195ff6700091c82fb267bb39
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-06 19:26:46 +0300

    Created the Header component from react-bootstrap's components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href='/'>MERN Simple Auth App</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ms-auto'>
+                            <Nav.Link href='/login'>
+                                <FaSignInAlt /> Sign In
+                            </Nav.Link>
+                            <Nav.Link href='/register'>
+                                <FaSignOutAlt /> Sign Out
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                    
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header;
