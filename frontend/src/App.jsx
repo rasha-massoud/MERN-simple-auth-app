ff2ecd417977e1840f67189681d7fa680e317137
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-06 20:26:25 +0300

    Generalized the App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
+import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
-import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
     <>
       <Header/>
-      <HomeScreen/>
+      <Outlet />
     </>
   )
 }
