770ddc11d68e1f3cee1eec7898538231c3d235e8
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-06 19:58:18 +0300

    Modified the Hero Component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,13 +3,13 @@ import { Container, Card, Button } from 'react-bootstrap'
 const Hero = () => {
     return (
         <div className='py-5'>
-            <Container className='d-flex justify-content-center'>
-                <Card className='p-5 d-flex flex-column lign-items-center hero-card bg-light w-75'>
+            <Container className='d-flex justify-content-center align-items-center'>
+                <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
                     <h1 className='text-center mb-4'>
                         MERN Authentications
                     </h1>
                     <p className='text-center mb-4'>
-                        This boilerplate stores a JWT in an HTTP-Only cookie for MERN authentication. The Redux Toolkit and the React Bootstrap Library are also utilized.
+                        This is a boilerplate that stores a JWT in an HTTP-Only cookie for MERN authentication. The Redux Toolkit and the React Bootstrap Library are also utilized.
                     </p>
                     <div className='d-flex'>
                         <Button variant='primary' href='/login' className='me-3'>
