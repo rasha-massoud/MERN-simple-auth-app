7713aac523e65f8ce614efeeba9a1351e56e0bc1
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-06 20:28:18 +0300

    Added a margin across the y-axis over the route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
+import { Container } from "react-bootstrap"
 import Header from "./components/Header"
 
 const App = () => {
   return (
     <>
-      <Header/>
-      <Outlet />
+      <Header />
+      <Container className="my-3">
+        <Outlet />
+      </Container>
     </>
   )
 }
