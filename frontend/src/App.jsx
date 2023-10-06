f610b57451a16da6b6a4801125fb786da951a05c
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-06 19:45:02 +0300

    Imported the Home Screen to the App.ksx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header"
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
     <>
       <Header/>
-      <h1>MERN Simple Auth App</h1>
+      <HomeScreen/>
     </>
   )
 }
