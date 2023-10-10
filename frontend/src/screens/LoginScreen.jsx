06afabbd3a21ed4a0c9b0299919a3beb74245033
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-10 11:26:39 +0300

    Added the Loader Spinner to the LoginScreen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -6,6 +6,7 @@ import FormContainer from '../components/FormContainer'
 import { useLoginMutation } from '../slices/usersApiSlice'
 import { setCredentials } from '../slices/authSlice'
 import { toast } from 'react-toastify'
+import Loader from '../components/LoaderSpinner'
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
@@ -51,6 +52,8 @@ const LoginScreen = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control type='password' placeholder='Enter Password' value={ password } onChange={ (e) => setPassword(e.target.value) }></Form.Control>
                 </Form.Group>
+
+                {isLoading && <Loader/> }
 
                 <Button type='submit' variant='primary' className='mt-3'>
                     Sign In
