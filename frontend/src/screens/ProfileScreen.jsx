2b56a484be7dfbc935d68bb93d5a7db4be4c8037
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-10 16:45:02 +0300

    Added the draft of the Profile Screen

---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCredentials } from '../slices/authSlice'
+import { useRegisterMutation } from '../slices/usersApiSlice'
+import FormContainer from '../components/FormContainer'
+import { toast } from 'react-toastify'
+import Loader from '../components/LoaderSpinner'
+
+const ProfileScreen = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { userInfo } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate('/');
+        }
+    }, [navigate, userInfo]);
+    
+    const submitHandler = async (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <FormContainer>
+            <h1>Update Profile</h1>
+            <Form onSubmit={ submitHandler }>
+                <Form.Group className='my-2' controlId='name'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type='text' placeholder='Enter Name' value={ name } onChange={ (e) => setName(e.target.value) }></Form.Control>
+                </Form.Group>
+
+                <Form.Group className='my-2' controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type='email' placeholder='Enter Email' value={ email } onChange={ (e) => setEmail(e.target.value) }></Form.Control>
+                </Form.Group>
+
+                <Form.Group className='my-2' controlId='password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type='password' placeholder='Enter Password' value={ password } onChange={ (e) => setPassword(e.target.value) }></Form.Control>
+                </Form.Group>
+
+                <Form.Group className='my-2' controlId='confirmPassword'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type='password' placeholder='Confirm Password' value={ confirmPassword } onChange={ (e) => setConfirmPassword(e.target.value) }></Form.Control>
+                </Form.Group>
+
+                {isLoading && <Loader/> }
+
+                <Button type='submit' variant='primary' className='mt-3'>
+                    Update
+                </Button>
+            </Form>
+        </FormContainer>
+    )
+}
+
+export default ProfileScreen;
