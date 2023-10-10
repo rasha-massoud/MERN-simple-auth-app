9ad07029012d133899b2228bb9c5a01265db0c75
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-10 17:03:02 +0300

    Fixed the route in the PrivateRoute Component

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 
 const PrivateRoute = () => {
     const { userInfo } = useSelector((state) => state.auth);
-    return userInfo ? <Outlet /> : <Navigate to='login' replace />
+    return userInfo ? <Outlet /> : <Navigate to='/login' replace />
 }
 
 export default PrivateRoute;
