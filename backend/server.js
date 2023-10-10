50b6fda160e07d2c566e992d76418722d0bff8d5
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-10 17:40:56 +0300

    Undid the changes done on the server.js on the frontend

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,7 @@ app.use(express.urlencoded({ extended : true}));
 app.use(cookiParser());
 app.use('/api/users', userRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-  
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
-} else {
-    app.get('/', (req, res) => res.send('Server is ready'));
-}
-
+app.get('/', (req, res) => res.send("Server is ready"));
 
 app.use(notFound);
 app.use(errorHandler);
