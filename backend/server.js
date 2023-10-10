e10610ad99759a11476caabafdbebf6364fadf28
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-10 17:45:25 +0300

    Updated the server.js file for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
@@ -17,7 +18,17 @@ app.use(express.urlencoded({ extended : true}));
 app.use(cookiParser());
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send("Server is ready"));
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => res.send("Server is ready"));
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
